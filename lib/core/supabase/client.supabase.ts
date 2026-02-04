da7e2823bfe018bf4200d2a76f58ce8dba584507
paltansjosess <paltansjosess@gmail.com>
2026-02-04 15:56:27 +0000

    🐛 fix(lib/core) : keys privadas .. pndjo

--- lib/core/supabase/client.supabase.ts
+++ lib/core/supabase/client.supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { storageAdapter } from '../storage/storage.adapter';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://sciqutnrtleysumeplno.supabase.co';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjaXF1dG5ydGxleXN1bWVwbG5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNDExNTcsImV4cCI6MjA4NTcxNzE1N30.AP_VByQR0QttMfbDDT-UbBZtlqF6BkOyboAcA8DJKo0';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
