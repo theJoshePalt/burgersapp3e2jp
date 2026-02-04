0aff214696658e223308a160a3e58203a549843e
paltansjosess <paltansjosess@gmail.com>
2026-02-04 16:44:12 +0000

    ✨ feat (app): Redireccionar pantallas
    Usaremos expo-router y nuestro useAuth para crear un sistema de protección.
    
    Se cambia _layout.tsx para que actúe como un "Portero".
    
    Si no tienes sesión, te manda al login.
    Si ya tienes sesión y tratas de ir al login, te manda al home o feed en este caso.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,33 @@
 import { usePushNotifications } from "@/lib/core/notifications/usePushNotifications";
 import { AuthProvider, useAuth } from "@/lib/modules/auth/AuthProvider";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
 
 function AuthLayout() {
-  const { session } = useAuth();
+  const { session, loading } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  // 1. Iniciamos el sistema de notificaciones
   const userId = session?.user.id;
   usePushNotifications(userId);
+
+  // 2. Efecto de Protección de Rutas
+  useEffect(() => {
+    if (loading) return; // Esperamos a saber si hay sesión o no
+
+    // Verificamos si estamos en el grupo de rutas de autenticación (login/register)
+    const inAuthGroup = segments[0] === '(auth)';
+
+    if (!session && !inAuthGroup) {
+      // ⛔ Si NO hay usuario y NO estamos en login -> Mandar a Login
+      router.replace('/(auth)/login');
+    } else if (session && inAuthGroup) {
+      // ✅ Si HAY usuario y estamos en login -> Mandar al Home
+      router.replace('/(tabs)/feed');
+    }
+  }, [session, loading, segments]);
+
   return <Stack />;
 }
 
