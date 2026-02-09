aa174aa7edc452a4b7cf081cd1f3e67676c54662
paltansjosess <paltansjosess@gmail.com>
2026-02-09 06:04:12 +0000

    🐛 fix(src/components/atoms) : CORRECCION EN EL BurgerModel y ahora ya no sale error en los pasos

--- src/components/atoms/BurgerModel.tsx
+++ src/components/atoms/BurgerModel.tsx
@@ -1,9 +1,28 @@
-import { Gltf } from '@react-three/drei/native';
 import React from 'react';
-import burgerModelPath from '../../../assets/models/burger.glb';
+import BurgerPart from './BurgerPart';
+import { BURGER_LAYERS } from '../../lib/core/logic/burgerLogic'; 
 
-export default function BurgerModel() {
+interface BurgerModelProps {
+  step: number;
+  isEaten: boolean;
+}
+
+export default function BurgerModel({ step, isEaten }: BurgerModelProps) {
   return (
-    <Gltf src = {burgerModelPath}/>
+    <group>
+      {BURGER_LAYERS.map((layer, index) => (
+        <BurgerPart 
+          key={layer.name}
+          // Solo se muestra si el paso actual es mayor o igual a su posición en el array
+          visible={step >= index + 1} 
+          positionY={layer.height} 
+          closedHeight={layer.closedHeight}
+          partName={layer.name} 
+          // Si estamos en el último paso (5), activamos la compactación
+          isFinished={step === 5}
+          isEaten={isEaten}
+        />
+      ))}
+    </group>
   );
 }
